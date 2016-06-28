1c23a6c3665e34c1eb6cb9547744945321601f7d
fidelscodes <fidelscodes@gmail.com>
2016-06-28 15:43:47 -0400

    Add solution using attr_accessor

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,28 @@
 require "pry"
 
 class Dog
-  # setter method
-  def name=(new_name)
-    @name = new_name
-  end
+  #practicing with attr_accessor
+  attr_accessor :name, :breed
+  @name = :name
+  @breed = :breed
 
-  # getter method
-  def name
-    @name
-  end
+  # # setter method
+  # def name=(new_name)
+  #   @name = new_name
+  # end
+  #
+  # # getter method
+  # def name
+  #   @name
+  # end
+  #
+  # def breed=(new_breed)
+  #   @breed = new_breed
+  # end
+  #
+  # def breed
+  #   @breed
+  # end
 
-  def breed=(new_breed)
-    @breed = new_breed
-  end
-
-  def breed
-    @breed
-  end
   # binding.pry
 end
