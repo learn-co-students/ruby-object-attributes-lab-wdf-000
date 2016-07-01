3da041fee46ccc072415dd654962c3a637f2d799
Esmery  Corniel <esmerycornielle@gmail.com>
2016-07-01 14:23:48 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  def name
+    @name
+  end
+
+  def name=(dogName)
+    @name = dogName
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(dogBreed)
+    @breed = dogBreed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name
+    @name
+  end
+
+  def name=(pName)
+    @name = pName
+  end
+
+  def job
+    @job
+  end
+
+  def job=(pJob)
+    @job = pJob
+  end
+end
