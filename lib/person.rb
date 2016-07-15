ed1d9742e82347163372f867edac50f6a58c304e
Mohammed Omar <samiomarmohammed@gmail.com>
2016-07-15 15:38:01 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  def first(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+  def second(job)
+    @job = job
+  end
+  def job
+    @job
+  end
+  def job=(new_job)
+    @job = new_job
+  end
+end
