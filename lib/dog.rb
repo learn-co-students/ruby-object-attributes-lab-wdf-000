099a09324a623afaaf112a0755d1a4d21c3fa7ef
dimacodes <dimitriserov@gmail.com>
2016-08-08 18:18:32 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+
+  def name
+    @name
+  end
+end
+
+class Dog
+
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+
+  def breed
+    @breed
+  end
+end
